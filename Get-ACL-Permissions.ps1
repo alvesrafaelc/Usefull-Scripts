d624a9685358af33eb6b28e79feb67deb3a2cf70
Rafael Alves <54069064+alvesrafaelc@users.noreply.github.com>
2022-05-18 12:17:13 -0300

    Add files via upload

---
+++ Get-ACL-Permissions.ps1
@@ -0,0 +1,13 @@
+﻿$FolderPath = dir -Directory -Path "\\AGPZSRFS02\Dados" -Force #Variavel para pegar mais 1 nivel de diretorio -Depth 1
+$Report = @()
+Foreach ($Folder in $FolderPath) {
+    $Acl = Get-Acl -Path $Folder.FullName
+    foreach ($Access in $acl.Access)
+        {
+            $Properties = [ordered]@{'FolderName'=$Folder.FullName;'AD
+Group or
+User'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+            $Report += New-Object -TypeName PSObject -Property $Properties
+        }
+}
+$Report | Export-Csv -path "C:\temp\FolderPermissions.csv"
